a9dc8f57190d27b96cde6030a2e2406b9d9fdc5e
DrakeDev23 <maccogoth@gmail.com>
2026-07-20 23:24:49 +0800

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-function App() {
-  return (
-    <div className="flex min-h-screen bg-red-500 items-center justify-center">
-      <h1 className="text-5xl font-bold text-blue-600">
-        ShopScout
-      </h1>
-    </div>
-  )
-}
+import Login from "./components/Login";
 
-export default App
+export default function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,10 @@
+export default function Login() {
+    return (
+        <div className="">
+            <p>
+                Find the items you need without the hassle of searching through countless shops. 
+                Discover products, explore stores, and save time with everything in one place.
+            </p>
+        </div>
+    )
+}
